77588ff3d2e2b62be75c0ad0d681b6476b840fa6
Noah Liu <noahxp@gmail.com>
2017-08-17 13:14:36 +0800

    v7 example

--- src/index.php
+++ src/index.php
@@ -1,6 +1,8 @@
 <?php
-echo "v6<br>";
-echo gethostname() . "<br>";
-echo gethostbyname(gethostname()) . "<br>";
+echo "v7   <br>";
+echo gethostname() . "   <br>";
+echo gethostbyname(gethostname()) . "   <br>";
+$date = new DateTime('2000-01-01');
+echo $date->format('Y-m-d H:i:s') . "  <br>";
 echo "\n";
 ?>
