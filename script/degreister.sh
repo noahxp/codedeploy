de41863a903a6fa4b38873aaddbd0e14f636ce54
Noah Liu <noahxp@gmail.com>
2017-08-17 15:07:09 +0800

    codedeploy example

--- script/degreister.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-
-DEBUG=true
-
-# Handle ASG processes
-# If HANDLE_PROCS=true, the following Auto Scaling events are suspended automatically during the deployment process: AZRebalance, AlarmNotification, ScheduledActions, ReplaceUnhealthy
-# reference : http://docs.aws.amazon.com/codedeploy/latest/userguide/integrations-aws-auto-scaling.html
-HANDLE_PROCS=true
